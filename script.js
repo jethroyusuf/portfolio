da4784ec0ba4558e985d405ca2c264ebba0e4df5
Yusuf Jethro Magaji <jethroyusuf17@gmail.com>
2026-02-09 23:07:31 +0100

    refactor: update HTML structure and enhance form functionality; improve CSS variables and responsiveness

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert("Welcome to my portfolio!")
+let button = document.getElementById('contactme');
+let nameInput = document.getElementById('name');
+let emailInput = document.getElementById('email');
+let body = document.querySelector('body')
+
+button.addEventListener('click', function() {
+ alert('my name is ' + nameInput.value + ' ' + emailInput.value) 
+ body.style.backgroundColor = 'brown'
+});
